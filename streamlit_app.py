dd5a8028584ed11ee777e0ccb8fb26002900e95d
nadyaaz23 <zahiranadya02@gmail.com>
2025-05-21 09:26:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("♥️ don best")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+kau bestiku yang cantik
